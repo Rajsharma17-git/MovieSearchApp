b1fa3f6664747d84b412ace8865526f1330c96e7
Rajsharma17-git <rajsharma17112005@gmail.com>
2025-02-28 22:28:44 +0530

    initial commit

---
+++ src/pages/Favorite.jsx
@@ -0,0 +1,10 @@
+import "../css/Favorites.css"
+
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>No Favorite Movies yet</h2>
+        <p>Start adding movies to Favorites and they appear here</p>
+    </div>
+}
+
+export default Favorites
---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+const API_KEY = "67fd4d583485b245b5389224e43cb79a";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+};
