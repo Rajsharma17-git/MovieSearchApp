f9fa5854106f6f00b38c4bb4dc550b4f3f96c807
Rajsharma17-git <rajsharma17112005@gmail.com>
2025-03-01 17:51:58 +0530

    Favorites

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,6 +1,24 @@
 import "../css/Favorites.css"
+import { useMovieContext } from "../contexts/MovieContext"
+import MovieCard from "../components/MovieCard"
 
 function Favorites() {
+    const {Favorites} = useMovieContext();
+
+    if (Favorites) {
+        return (
+            <div className="favorites">
+                <h2>Your Favorites</h2>
+            <div className="movies-grid">
+            {Favorites.map((movie) => (
+                 
+                <MovieCard movie={movie} key={movie.id} />
+                
+            ))}
+        </div>
+        </div>
+    )}
+
     return <div className="favorites-empty">
         <h2>No Favorite Movies yet</h2>
         <p>Start adding movies to Favorites and they appear here</p>
